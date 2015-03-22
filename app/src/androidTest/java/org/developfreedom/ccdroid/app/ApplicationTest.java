da9f84b0f48fd69b649f7bbf5e2f23aea2d3df51
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-03-22 20:31:55 +0530

    Setup test framework

--- app/src/androidTest/java/org/developfreedom/ccdroid/app/ApplicationTest.java
+++ app/src/androidTest/java/org/developfreedom/ccdroid/app/ApplicationTest.java
@@ -2,6 +2,7 @@ package org.developfreedom.ccdroid.app;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import org.junit.Test;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +10,11 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @Test
+    public void testTrueIsTrue() {
+        assertEquals(true, true);
+        assertEquals(false, true);
     }
 }
