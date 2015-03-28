3e41439f1e7af22aa55badec8add68b226458803
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-03-29 03:29:30 +0530

    Fix lombok AllArgsConstructor compile time error

--- app/src/main/java/org/developfreedom/ccdroid/app/Project.java
+++ app/src/main/java/org/developfreedom/ccdroid/app/Project.java
@@ -1,13 +1,14 @@
 package org.developfreedom.ccdroid.app;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-//@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor(suppressConstructorProperties = true)
 public class Project {
     String name;
     String activity;
@@ -15,13 +16,4 @@ public class Project {
     String lastBuildStatus;
     String lastBuildTime;
     String webUrl;
-
-    public Project(String a, String b, String c, String d, String e, String f) {
-        this.name = a;
-        this.activity = b;
-        this.lastBuildLabel = c;
-        this.lastBuildStatus = d;
-        this.lastBuildTime = e;
-        this.webUrl = f;
-    }
 }
