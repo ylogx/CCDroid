6e6310458d4284a41fbaec1721e62a6260aeda26
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-06-06 17:38:34 +0530

    Add licensing information

--- app/src/main/java/org/developfreedom/ccdroid/app/MainActivity.java
+++ app/src/main/java/org/developfreedom/ccdroid/app/MainActivity.java
@@ -1,3 +1,23 @@
+/**
+ * CCDroid - An open source build monitor for Android
+ * <p/>
+ * Copyright (c) 2015 Shubham Chaudhary <me@shubhamchaudhary.in>
+ * <p/>
+ * This file is part of CCDroid.
+ * <p/>
+ * CCDroid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * CCDroid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with CCDroid.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.developfreedom.ccdroid.app;
 
 import android.app.Activity;
