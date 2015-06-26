0808959d1db0b74aa7ae068fdc0e74421c4f3b4a
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-06-26 06:26:50 +0530

    Add stub authenticator for sync adapter

---
+++ app/src/main/java/org/developfreedom/ccdroid/app/sync/AuthenticatorService.java
@@ -0,0 +1,45 @@
+/*
+ * AuthenticatorService.java
+ *
+ * Copyright (c) 2015 Shubham Chaudhary <me@shubhamchaudhary.in>
+ *
+ * CCDroid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CCDroid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CCDroid.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.developfreedom.ccdroid.app.sync;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class AuthenticatorService extends Service {
+    // Instance field that stores the authenticator object
+    private StubAuthenticator mAuthenticator;
+
+    @Override
+    public void onCreate() {
+        // Create a new authenticator object
+        mAuthenticator = new StubAuthenticator(this);
+    }
+
+    /*
+     * When the system binds to this Service to make the RPC call
+     * return the authenticator's IBinder.
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mAuthenticator.getIBinder();
+    }
+}
---
+++ app/src/main/java/org/developfreedom/ccdroid/app/sync/StubAuthenticator.java
@@ -0,0 +1,98 @@
+/*
+ * StubAuthenticator.java
+ *
+ * Copyright (c) 2015 Shubham Chaudhary <me@shubhamchaudhary.in>
+ *
+ * CCDroid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CCDroid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CCDroid.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.developfreedom.ccdroid.app.sync;
+
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.NetworkErrorException;
+import android.content.Context;
+import android.os.Bundle;
+
+/*
+ * Implement AbstractAccountAuthenticator and stub out all
+ * of its methods
+ */
+public class StubAuthenticator extends AbstractAccountAuthenticator {
+    // Simple constructor
+    public StubAuthenticator(Context context) {
+        super(context);
+    }
+
+    // Editing properties is not supported
+    @Override
+    public Bundle editProperties(
+            AccountAuthenticatorResponse r, String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    // Don't add additional accounts
+    @Override
+    public Bundle addAccount(
+            AccountAuthenticatorResponse r,
+            String s,
+            String s2,
+            String[] strings,
+            Bundle bundle) throws NetworkErrorException {
+        return null;
+    }
+
+    // Ignore attempts to confirm credentials
+    @Override
+    public Bundle confirmCredentials(
+            AccountAuthenticatorResponse r,
+            Account account,
+            Bundle bundle) throws NetworkErrorException {
+        return null;
+    }
+
+    // Getting an authentication token is not supported
+    @Override
+    public Bundle getAuthToken(
+            AccountAuthenticatorResponse r,
+            Account account,
+            String s,
+            Bundle bundle) throws NetworkErrorException {
+        throw new UnsupportedOperationException();
+    }
+
+    // Getting a label for the auth token is not supported
+    @Override
+    public String getAuthTokenLabel(String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    // Updating user credentials is not supported
+    @Override
+    public Bundle updateCredentials(
+            AccountAuthenticatorResponse r,
+            Account account,
+            String s, Bundle bundle) throws NetworkErrorException {
+        throw new UnsupportedOperationException();
+    }
+
+    // Checking features for the account is not supported
+    @Override
+    public Bundle hasFeatures(
+            AccountAuthenticatorResponse r,
+            Account account, String[] strings) throws NetworkErrorException {
+        throw new UnsupportedOperationException();
+    }
+}
