3b7327cc03f9be3edf80016115cada5116b1dbb8
Shubham Chaudhary <me@shubhamchaudhary.in>
2016-03-25 04:44:36 +0530

    Add more tests

--- app/src/test/java/org/developfreedom/ccdroid/app/XmlFeedReaderTest.java
+++ app/src/test/java/org/developfreedom/ccdroid/app/XmlFeedReaderTest.java
@@ -22,13 +22,30 @@ public class XmlFeedReaderTest {
             "  <Project name=\"DevelopFreedom/logmein-android (master) :: AndroidInstall\" activity=\"Sleeping\" lastBuildLabel=\"7\" lastBuildStatus=\"Success\" lastBuildTime=\"2015-07-27T21:08:18Z\" webUrl=\"https://snap-ci.com/DevelopFreedom/logmein-android/branch/master/logs/defaultPipeline/7/AndroidInstall\"/>\n" +
             "  <Project name=\"DevelopFreedom/logmein-android (master) :: TestRunnar\" activity=\"Sleeping\" lastBuildLabel=\"7\" lastBuildStatus=\"Failure\" lastBuildTime=\"2015-07-27T21:08:23Z\" webUrl=\"https://snap-ci.com/DevelopFreedom/logmein-android/branch/master/logs/defaultPipeline/7/TestRunnar\"/>\n" +
             "</Projects>\n";
-    @Test public void parse() throws Exception {
+    private static final String PROJECT_XML_1_WITH_SKIP = "<Projects>\n" +
+            "  <SkipableTag foo=\"bar\" baz=\"booze\"/>\n" +
+            "  <Project name=\"DevelopFreedom/logmein-android (master) :: AndroidInstall\" activity=\"Sleeping\" lastBuildLabel=\"7\" lastBuildStatus=\"Success\" lastBuildTime=\"2015-07-27T21:08:18Z\" webUrl=\"https://snap-ci.com/DevelopFreedom/logmein-android/branch/master/logs/defaultPipeline/7/AndroidInstall\"/>\n" +
+            "</Projects>\n";
+
+    @Test public void parse_HasTwoProject_ConvertToPOJO() throws Exception {
         InputStream stream = new ByteArrayInputStream(PROJECT_XML_2.getBytes());
 
         List projects = new XmlFeedReader().parse(stream);
 
         assertThat(projects).hasSize(2).hasOnlyElementsOfType(Project.class);
-        Project project = (Project) projects.get(0);
+        assertThatProjectParsedCorrectly((Project) projects.get(0));
+    }
+
+    @Test public void parse_HasSkipableTag_SkipOver() throws Exception {
+        InputStream stream = new ByteArrayInputStream(PROJECT_XML_1_WITH_SKIP.getBytes());
+
+        List projects = new XmlFeedReader().parse(stream);
+
+        assertThat(projects).hasSize(1).hasOnlyElementsOfType(Project.class);
+        assertThatProjectParsedCorrectly((Project) projects.get(0));
+    }
+
+    private void assertThatProjectParsedCorrectly(Project project) {
         assertThat(project).isNotNull();
         assertThat(project.getName()).isEqualTo("DevelopFreedom/logmein-android (master) :: AndroidInstall");
         assertThat(project.getActivity()).isEqualTo("Sleeping");
